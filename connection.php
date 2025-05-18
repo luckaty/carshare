732c2ce306b3d3875a260d0c7f750ec5ee27298c
luckaty <132684540+luckaty@users.noreply.github.com>
2025-05-18 17:27:39 -0400

    Update connection.php

--- connection.php
+++ connection.php
@@ -3,6 +3,6 @@
 $link = mysqli_connect("localhost", "user", "your database password", "database name");
 if(mysqli_connect_error()){
     die('ERROR: Unable to connect:' . mysqli_connect_error()); 
-    echo "<script>window.alert('Hi!')</script>";
+    echo "<script>window.alert('Could not connect to database')</script>";
 }
     ?>
